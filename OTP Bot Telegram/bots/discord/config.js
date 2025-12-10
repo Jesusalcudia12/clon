9357ff5c6cd225e8094038c0f76c4aed5b402787
Jesusalcudia12 <132318909+Jesusalcudia12@users.noreply.github.com>
2025-12-10 05:12:35 -0600

    Update config.js

--- OTP Bot Telegram/bots/discord/config.js
+++ OTP Bot Telegram/bots/discord/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    apiurl: '',
+    apiurl: 'https://discord.com/oauth2/authorize?client_id=1448264293920276654&permissions=8&integration_type=0&scope=bot',
     apipassword: '',
 
-    discordtoken: '',
+    discordtoken: '1448264293920276654',
     discordprefix: '.',
     secretpassword: '',
     botuser_rolename: 'Bot User',
